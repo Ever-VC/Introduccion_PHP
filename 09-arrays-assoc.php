3cbe53ba95f48e4c14af9ee76f81a0392decf12b
Ever-VC <ever.vasquezc27@gmail.com>
2024-12-30 20:34:24 -0600

    💥 NEW: Arreglos asociativos

--- 09-arrays-assoc.php
+++ 09-arrays-assoc.php
@@ -1,6 +1,42 @@
 <?php include 'includes/header.php';
 
+$cliente = [
+    'nombre' => 'Juan',
+    'saldo' => 200,
+    'informacion' => [
+        'tipo' => 'Premium',
+        'disponible' => 100
+    ]
+];
+
+echo "<pre>";
+var_dump($cliente);
+echo "</pre>";
+
+echo $cliente['nombre'];
+
+echo $cliente['informacion']['tipo'];
+
+// Agregar elementos a un array
+$cliente['codigo'] = 1234;
+
+echo "<pre>";
+var_dump($cliente);
+echo "</pre>";
+
+// Modificar elementos de un array
+$cliente['nombre'] = 'Alberto';
+
+echo "<pre>";
+var_dump($cliente);
+echo "</pre>";
 
 
+// Eliminar elementos de un array
+unset($cliente['informacion']);
+
+echo "<pre>";
+var_dump($cliente);
+echo "</pre>";
 
 include 'includes/footer.php';
