c2541a2baa7559378286b0aa94428d6a7a6fb7d3
Ever-VC <ever.vasquezc27@gmail.com>
2024-12-30 19:51:38 -0600

    💥 NEW: Tipos de datos

--- 03-tipos-dato.php
+++ 03-tipos-dato.php
@@ -1,6 +1,24 @@
 <?php include 'includes/header.php';
 
+// Boolean
+$logueado = true;
+var_dump($logueado);
 
+// Entero
+$numero = 200;
+var_dump($numero);
+
+// Float
+$numero_decimal = 200.5;
+var_dump($numero_decimal);
+
+// String
+$nombre = 'Ever';
+var_dump($nombre);
+
+// Arreglo
+$colores = array('Rojo', 'Azul', 'Verde');
+var_dump($colores);
 
 
 include 'includes/footer.php';
