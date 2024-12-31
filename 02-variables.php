a91499386324a3aa6931c2256c897a6ac3dc06f8
Ever-VC <ever.vasquezc27@gmail.com>
2024-12-30 19:48:47 -0600

    💥 NEW: Estructura de una variable en PHP

--- 02-variables.php
+++ 02-variables.php
@@ -1,6 +1,26 @@
 <?php include 'includes/header.php';
 
+// Crear variables
+$nombre = 'Ever';
+$apellido = 'Vasquez';
 
+// Concatenar
+echo $nombre . ' ' . $apellido;
 
+// Cambiar el valor de la variable
+$nombre = 'Ever 2';
+
+// Imprimir
+echo $nombre;
+
+// Constantes
+define('constante', 'Valor de la constante');
+echo constante;
+
+const constante2 = 'Valor de la constante 2';
+echo constante2;
+
+// Constantes predefinidas
+echo PHP_VERSION;
 
 include 'includes/footer.php';
