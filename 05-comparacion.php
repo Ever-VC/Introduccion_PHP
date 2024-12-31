2f5af93cbf787b9b197ffecf628279db44db20f1
Ever-VC <ever.vasquezc27@gmail.com>
2024-12-30 20:01:04 -0600

    💥 NEW: Operadores lógicos

--- 05-comparacion.php
+++ 05-comparacion.php
@@ -1,7 +1,32 @@
 <?php include 'includes/header.php';
 
+$numero1 = 20;
+$numero2 = 30;
+$numero3 = 30;
+$numero4 = "30";
 
+var_dump($numero1 > $numero2);
+echo "<br>";
 
+var_dump($numero1 < $numero2);
+echo "<br>";
 
+var_dump($numero1 >= $numero2);
+echo "<br>";
+
+var_dump($numero1 <= $numero2);
+echo "<br>";
+
+var_dump($numero2 == $numero3);
+echo "<br>";
+
+var_dump($numero2 == $numero4);
+echo "<br>";
+
+var_dump($numero2 === $numero4);
+echo "<br>";
+
+// Retorna -1 si el primer número es menor, 0 si son iguales y 1 si el primer número es mayor
+var_dump($numero1 <=> $numero2); 
 
 include 'includes/footer.php';
