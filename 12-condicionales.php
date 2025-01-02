2437a496db8637304460d740b80eba9ddee314f6
Ever-VC <ever.vasquezc27@gmail.com>
2025-01-02 17:59:40 -0600

    💥 NEW: if, else if, else y switch

--- 12-condicionales.php
+++ 12-condicionales.php
@@ -1,7 +1,66 @@
 <?php include 'includes/header.php';
 
+$autenticado = true;
+$admin = false;
 
+if($autenticado || $admin) {
+    echo "El usuario esta autenticado";
+} else {
+    echo "El usuario no esta autenticado, inicia sesion"; 
+}
 
+echo "<br>";
 
+// If anidado
+$cliente = [
+    'nombre' => 'Ever',
+    'saldo' => 200,
+    'informacion' => [
+        'tipo' => 'Premium'
+    ]
+];
+
+if (!empty($cliente)) {
+    echo "El arreglo de cliente no esta vacio";
+    echo "<br>";
+    if ($cliente['saldo'] > 0) {
+        echo "El cliente tiene saldo";
+    } else {
+        echo "No hay saldo";
+    }
+} else {
+    echo "No hay cliente";
+}
+
+echo "<br>";
+
+// Else if
+if ($cliente['saldo'] > 0) {
+    echo "El cliente tiene saldo";
+} else if ($cliente['informacion']['tipo'] === 'Premium') {
+    echo "El cliente es Premium";
+} else {
+    echo "No hay cliente";
+}
+
+echo "<br>";
+
+// Switch
+$tecnologia = 'HTML';
+
+switch ($tecnologia) {
+    case 'PHP':
+        echo "Desarrollo web con PHP";
+        break;
+    case 'JavaScript':
+        echo "Desarrollo web con JavaScript";
+        break;
+    case 'HTML':
+        echo "Desarrollo web con HTML";
+        break;
+    default:
+        echo "Desarrollo web";
+        break;
+}
 
 include 'includes/footer.php';
