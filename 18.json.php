de3e8d06328f311ed7883893eb130dd8e6d84359
Ever-VC <ever.vasquezc27@gmail.com>
2025-01-02 19:37:59 -0600

    💥 NEW: Pasando de arregflos a Json y viceversa

--- 18.json.php
+++ 18.json.php
@@ -1,6 +1,31 @@
 <?php include 'includes/header.php';
 
+$productos = [
+    [
+        'nombre' => 'Tablet',
+        'precio' => 200,
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'Televisión 24"',
+        'precio' => 300,
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'Monitor Curvi MSI 27"',
+        'precio' => 400,
+        'disponible' => false
+    ]
+];
 
+echo '<pre>';
+//var_dump($productos);
+$json = json_encode($productos, JSON_UNESCAPED_UNICODE);
+//evar_dump($json);
+
+$json_array = json_decode($json, true);
+var_dump($json_array);
+echo '</pre>';
 
 
 
